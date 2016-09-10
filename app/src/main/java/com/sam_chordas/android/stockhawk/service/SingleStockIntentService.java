c7ead3f6232cdb2f3168cc0b8936e0394093976e
parth12 <parthpanchal12196@gmail.com>
2016-09-10 11:33:29 +0530

    Initial commit

---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/service/SingleStockIntentService.java
@@ -0,0 +1,44 @@
+package com.sam_chordas.android.stockhawk.service;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.gcm.TaskParams;
+
+/**
+ * Created by parth panchal on 25-07-2016.
+ */
+public class SingleStockIntentService extends IntentService {
+
+    private String LOG_TAG = SingleStockIntentService.class.getSimpleName();
+
+    public SingleStockIntentService() {
+        super(SingleStockIntentService.class.getName());
+    }
+
+    public SingleStockIntentService(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+        Log.d(LOG_TAG, "Historic Stock Intent Service");
+
+        Bundle args = new Bundle();
+
+        args.putString(SingleStockTaskService.PARAMS_EXTRA_SYMBOL,
+                intent.getStringExtra(SingleStockTaskService.PARAMS_EXTRA_SYMBOL));
+        args.putString(SingleStockTaskService.PARAMS_EXTRA_START_DATE,
+                intent.getStringExtra(SingleStockTaskService.PARAMS_EXTRA_START_DATE));
+        args.putString(SingleStockTaskService.PARAMS_EXTRA_END_DATE,
+                intent.getStringExtra(SingleStockTaskService.PARAMS_EXTRA_END_DATE));
+
+        // Initiates the task service
+        SingleStockTaskService taskService = new SingleStockTaskService();
+        taskService.onRunTask(new TaskParams(SingleStockTaskService.PARAMS_TAG, args));
+
+    }
+}
---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/widget/WidgetProvider.java
@@ -0,0 +1,97 @@
+package com.sam_chordas.android.stockhawk.widget;
+
+/**
+ * Created by parth panchal on 25-07-2016.
+ */
+
+import android.content.Context;
+
+import android.annotation.TargetApi;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.TaskStackBuilder;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+import com.sam_chordas.android.stockhawk.R;
+import com.sam_chordas.android.stockhawk.rest.CursorRecyclerViewAdapter;
+import com.sam_chordas.android.stockhawk.ui.MyStocksActivity;
+import com.sam_chordas.android.stockhawk.ui.StockDetailActivity;
+
+public class WidgetProvider extends AppWidgetProvider {
+
+    private String LOG_TAG = WidgetProvider.class.getSimpleName();
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+        for (int appWidgetId : appWidgetIds) {
+
+            RemoteViews views = new RemoteViews(
+                    context.getPackageName(),
+                    R.layout.widget);
+
+            Intent intent = new Intent(context, MyStocksActivity.class);
+            PendingIntent pendingIntent = PendingIntent
+                    .getActivity(context, 0, intent, 0);
+            views.setOnClickPendingIntent(R.id.collection_widget_title_textview, pendingIntent);
+
+            // Calls the RemoteViewsService, which creates a RemoteViewsFactory
+            // which works as an adapter for the list view
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+                setRemoteAdapter(context, views);
+            } else {
+                setRemoteAdapterV11(context, views);
+            }
+
+            // Creates an intent template to open StockDetailsActivity when user
+            // clicks on a specific stock
+            Intent clickIntentTemplate = new Intent(context, StockDetailActivity.class);
+
+            PendingIntent clickPendingIntentTemplate = TaskStackBuilder.create(context)
+                    .addNextIntentWithParentStack(clickIntentTemplate)
+                    .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+            views.setPendingIntentTemplate(R.id.collection_widget_listview, clickPendingIntentTemplate);
+
+            views.setEmptyView(R.id.collection_widget_listview, R.id.collection_widget_empty_textview);
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+
+        if (AppWidgetManager.ACTION_APPWIDGET_UPDATE.equals(intent.getAction())) {
+
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            int[] appWidgetIds = appWidgetManager.getAppWidgetIds(
+                    new ComponentName(context, getClass()));
+            appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.collection_widget_listview);
+        }
+
+    }
+
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    private void setRemoteAdapter(Context context, @NonNull final RemoteViews views) {
+        views.setRemoteAdapter(R.id.collection_widget_listview,
+                new Intent(context, WidgetRemoveViewsService.class));
+    }
+
+    @SuppressWarnings("deprecation")
+    private void setRemoteAdapterV11(Context context, @NonNull final RemoteViews views) {
+        views.setRemoteAdapter(0, R.id.collection_widget_listview,
+                new Intent(context, WidgetRemoveViewsService.class));
+    }
+
+}
